249f35c07f24001cb9697dce0d6a64d124e5b545
astubbings <53932400+astubbings@users.noreply.github.com>
2019-10-17 18:29:26 -0600

    final MVP submit for airtable clean and tight code

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -34,9 +34,31 @@ class Student extends Person {
             this.favSubjects = attrStu.favSubjects
     }
     listsSubjects() {
-        array.forEach(this.favSubjects)
+        //return this.favSubjects;
+        this.favSubjects.forEach (itemsInArray => console.log(itemsInArray));
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject){
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
+
+class ProjectManagers extends Instructor {
+    constructor(attrPM) {
+        super(attrPM);
+        this.gradClassName = attrPM.gradClassName,
+        this.favInstructor = attrPM.favInstructor
+    }
+    standUp(channel){
+        return `${this.name} announces to ${channel}, @${channel} standy times!​​​​​`;
+    }
+    debugsCode(Student,subject){
+        return `${this.name} debugs ${Student.name}'s code on ${subject}`;
+    }
+}
+
 
 
 const brit = new Instructor({
@@ -63,9 +85,31 @@ const franklin = new Student({
     age: 24,
     previousBackground: 'WhatABurger Emp',
     className: 'JS 101',
-    favSubjects: ['pottery', 'math', 'programming']
+    favSubjects: ['pottery', 'biology', 'Lambda DS']
 });
 
+const mike = new Student({
+    name: 'Mike',
+    location: 'Los Santos',
+    age: 45,
+    previousBackground: 'FIB consultant',
+    className: 'JS 103',
+    favSubjects: ['pottery', 'biology', 'Lambda DS']
+});
 
-  console.log(brit.demo('Javascript-IV'));
-  console.log(jerry.listsSubjects());
+const josh = new ProjectManagers({
+    name: 'Josh',
+    location: 'Utah',
+    age: 35,
+    gradClassName: 'CS1',
+    favInstructor: brit
+});
+
+console.log(mike.speak());
+console.log(brit.demo('Javascript-IV'));
+console.log(brit.grade(mike, 'build week unit 2'));
+jerry.listsSubjects();
+console.log(franklin.PRAssignment('React Week 2'));
+console.log(mike.sprintChallenge('Advanced Javascript'));
+console.log(josh.standUp('#Web25_help'));
+console.log(josh.debugsCode(franklin, 'Comp Sci 1'));
